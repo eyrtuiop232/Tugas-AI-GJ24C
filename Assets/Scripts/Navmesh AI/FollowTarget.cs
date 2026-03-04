6db7db3f4e9b3bd7b54e630603b497bfb2225412
eyrtuiop232 <gamersgub@gmail.com>
2026-03-05 02:32:18 +0700

    added navmesh & 2d & navmesh ai

---
+++ Assets/Scripts/Navmesh AI/FollowTarget.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class FollowTarget : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
